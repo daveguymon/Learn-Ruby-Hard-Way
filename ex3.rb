24f90096226af07f3cda5ea65a7e5e8277ed1c48
Dave Guymon <David.Guymon@mx.com>
2017-10-31 15:13:17 -0600

    Completed Exercises 2-3

---
+++ ex3.rb
@@ -0,0 +1,37 @@
+#Prints a string of text
+puts "I will now count my chickens:"
+
+#Prints a string, which contains a computation. The result of the computation replaces the code between the curly brackets
+puts "Hens #{25 + 30 / 6}"
+#Also runs a computation and prints a string containing the result
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#Prints a string of text
+puts "Now I will count the eggs:"
+
+#Performs mathematical computation following the order of operations
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Prints a string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Prints a boolean value, in this case false
+puts 3 + 2 < 5 - 7
+
+#Prints a string which contains the result of a computation using addition
+puts "What is 3 + 2? #{3 + 2}"
+#Prints a string which contains the result of a computation using subtraction
+puts "What is 5 - 7? #{5 - 7}"
+
+#Print a string of text
+puts "Oh, that's why it's false."
+
+#Prints another string of text
+puts "How about some more?"
+
+#Prints a string containing a boolean value resulting from comparison using the greater-than operator
+puts "Is it greater? #{5 > -2}"
+#Prints a string containing a boolean value resulting from the greater-than-or-equal-to operator
+puts "Is it greater or equal? #{5 >= -2}"
+#Prints a string containing a boolean value resulting from the less-than-or-equal-to operator
+puts "Is it less or equal? #{5 <= -2}"
