151ef47fa9d7cfec1ebb43e45509dcc3bb8df0a4
Dave Guymon <David.Guymon@mx.com>
2017-11-01 16:10:09 -0600

    Completed exercise 5

---
+++ ex5.rb
@@ -0,0 +1,14 @@
+my_name = 'Dave Guymon'
+my_age = 30
+my_height = 74
+my_weight = 220
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on what he has been eating."
