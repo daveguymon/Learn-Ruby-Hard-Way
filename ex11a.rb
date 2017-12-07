f0661f859ecfd4d491faa9d27a58e78a33635d27
Dave Guymon <David.Guymon@mx.com>
2017-12-07 16:31:50 -0700

    Exercises 10-11

---
+++ ex11a.rb
@@ -0,0 +1,8 @@
+print "What is your name? "
+name = gets.chomp
+print "What is your quest? "
+quest = gets
+print "What is the wind velocity of a sparrow?"
+velocity = gets.chomp
+
+puts "Your name is #{name}. Your quest is #{quest}. The velocity is #{velocity}."
